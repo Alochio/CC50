e0a054a9dd0489aac17af2ef7452d4a5a38ba8f8
Vinicius Alochio <76494341+Alochio@users.noreply.github.com>
2023-09-12 20:55:18 -0300

    update exercicio 08 - em desenvolvimento

---
+++ exercicio08_Scrabble/Substituição/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "substituicao.h"
+
+int main()
+{
+
+    char chave[26];
+    int analizeChave;
+
+    printf("Digite a chave de criptografia: ");
+    scanf("%s", chave);
+
+    analizeChave = validaChave(chave);
+
+    if(analizeChave == 1)
+        auxConvert(chave);
+    else
+        printf("Fim do programa!\n");
+
+    return 0;
+}
---
+++ exercicio08_Scrabble/Substituição/substituicao.h
@@ -0,0 +1,9 @@
+#ifndef SUBSTITUICAO_H_INCLUDED
+#define SUBSTITUICAO_H_INCLUDED
+
+int validaChave(char chave[]);
+void auxConvert(char chave[]);
+void textoSimples(char chave[], int vetorResultante[]);
+
+
+#endif // SUBSTITUICAO_H_INCLUDED
