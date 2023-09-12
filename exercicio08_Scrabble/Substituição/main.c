165c688eb0d906295537c8623e6f8b92578f05a8
Vinicius Alochio <76494341+Alochio@users.noreply.github.com>
2023-09-12 20:52:30 -0300

    Atualização exercicio 08 - em desenvolvimento

---
+++ exercicio08_Scrabble/Substituição/main.c
@@ -0,0 +1,45 @@
+<<<<<<< HEAD
+#include <stdio.h>
+#include <stdlib.h>
+#include "substituicao.h"
+
+int main()
+{
+
+    char chave[26];
+    int analizeChave;
+
+    printf("Digite a chave de criptografia: ");
+    scanf("%s", chave);
+
+    analizeChave = validaChave(chave);
+
+    if(analizeChave == 1)
+        auxConvert(chave);
+    else
+        printf("Fim do programa!\n");
+
+    return 0;
+}
+
+=======
+#include <stdio.h>
+#include <stdlib.h>
+#include "substituicao.h"
+
+int main()
+{
+
+
+    char chave[26];
+
+    printf("\n\nDigite a chave de criptografia: ");
+    scanf("%s", chave);
+    validaChave(chave);
+
+    textoSimples(chave);
+
+    return 0;
+}
+
+>>>>>>> 75ad35fc9baae2a24d1cb7946a2d0c17e4737d88
---
+++ exercicio08_Scrabble/Substituição/substituicao.h
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+#ifndef SUBSTITUICAO_H_INCLUDED
+#define SUBSTITUICAO_H_INCLUDED
+
+int validaChave(char chave[]);
+void auxConvert(char chave[]);
+void textoSimples(char chave[], int vetorResultante[]);
+
+
+#endif // SUBSTITUICAO_H_INCLUDED
+=======
+#ifndef SUBSTITUICAO_H_INCLUDED
+#define SUBSTITUICAO_H_INCLUDED
+
+void validaChave(char chave[]);
+void textoSimples(char chave[]);
+
+
+#endif // SUBSTITUICAO_H_INCLUDED
+>>>>>>> 75ad35fc9baae2a24d1cb7946a2d0c17e4737d88
