378273a73cf481f8ec134cc2789a772ee5087227
Vinicius Alochio <76494341+Alochio@users.noreply.github.com>
2023-09-12 23:31:09 -0300

    Delete exercicio08_Scrabble/Substituição directory

--- exercicio08_Scrabble/Substituição/substituicao.h
+++
@@ -1,9 +0,0 @@
-#ifndef SUBSTITUICAO_H_INCLUDED
-#define SUBSTITUICAO_H_INCLUDED
-
-int validaChave(char chave[]);
-void auxConvert(char chave[]);
-void textoSimples(char chave[], int vetorResultante[]);
-
-
-#endif // SUBSTITUICAO_H_INCLUDED
